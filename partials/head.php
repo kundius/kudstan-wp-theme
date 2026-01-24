cd38d86e33d5d8b26ebf01d0d0e2f130032367c6
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-01-24 22:13:51 +0300

    123

--- partials/head.php
+++ partials/head.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
 <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" type="image/x-icon">
+<link rel="icon" href="<?php bloginfo('template_url'); ?>/favicon.svg" type="image/svg+xml">
 <meta name="yandex-verification" content="" />
 <meta name="cmsmagazine" content="ef9002bde22164e1bb8ca1b1793cf47b" />
 <meta itemprop="name" content="<?php bloginfo('blogname'); ?>" />
